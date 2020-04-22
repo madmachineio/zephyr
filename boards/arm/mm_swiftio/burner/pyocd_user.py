3ff3f3053bae83aefca9e905c65783c287c4c385
Frank Li <lgl88911@163.com>
2020-04-22 22:58:57 +0800

    update pyocd cmd for west cmd
    Signed-off-by: Frank Li <lgl88911@163.com>

--- boards/arm/mm_swiftio/burner/pyocd_user.py
+++ boards/arm/mm_swiftio/burner/pyocd_user.py
@@ -9,8 +9,9 @@ def will_connect():
 
     # 'target' is global
     # pylint: disable=undefined-variable
-    extFlash = target.memory_map.get_region_by_name("flexspi")
+    extFlash = target.memory_map.get_first_matching_region(name="flexspi")
 
     # Set the path to an .FLM flash algorithm.
     flm_path = os.getenv('ZEPHYR_BASE') + '/boards/arm/mm_swiftio/burner/MIMXRT105x_QuadSPI_4KB_SEC.FLM'
+
     extFlash.flm = flm_path

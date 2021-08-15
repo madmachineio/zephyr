767100c9c2639dc2ec5795b1d2690df595b9ab49
Frank Li <lgl88911@163.com>
2021-08-15 15:28:52 +0800

    Support newlib filesystem
    1. Add fs io porting API _open, _unlink.
    2. Add dirent type support.
    
    Signed-off-by: Frank Li <lgl88911@163.com>

--- include/posix/dirent.h
+++ include/posix/dirent.h
@@ -18,8 +18,31 @@ extern "C" {
 
 typedef void DIR;
 
+enum {
+	DT_UNKNOWN      = 0,
+# define DT_UNKNOWN        DT_UNKNOWN
+	DT_FIFO         = 1,
+# define DT_FIFO        DT_FIFO
+	DT_CHR          = 2,
+# define DT_CHR                DT_CHR
+	DT_DIR          = 4,
+# define DT_DIR                DT_DIR
+	DT_BLK          = 6,
+# define DT_BLK                DT_BLK
+	DT_REG          = 8,
+# define DT_REG                DT_REG
+	DT_LNK          = 10,
+# define DT_LNK                DT_LNK
+	DT_SOCK         = 12,
+# define DT_SOCK        DT_SOCK
+	DT_WHT          = 14
+# define DT_WHT                DT_WHT
+};
+
+
 struct dirent {
 	unsigned int d_ino;
+	unsigned char d_type;
 	char d_name[PATH_MAX + 1];
 };
 
@@ -32,6 +55,6 @@ extern struct dirent *readdir(DIR *dirp);
 }
 #endif
 
-#endif /* CONFIG_POSIX_FS */
+#endif  /* CONFIG_POSIX_FS */
 
-#endif	/* ZEPHYR_INCLUDE_POSIX_DIRENT_H_ */
+#endif  /* ZEPHYR_INCLUDE_POSIX_DIRENT_H_ */
